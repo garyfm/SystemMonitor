97871682c4bcafc5b6ad735ad796bca954f66c50
garyfm <garyfmullen@gmail.com>
2021-05-26 07:04:34 -0700

    Moving the curser with the arrow keys and highligthing the current line

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,12 @@
 
 #define NUM_FIELDS (10)
 
-std::array<std::string, NUM_FIELDS> process_fields = {"Name","Pid", "User", "State", "Threads", "Start Time", "CPU Time", "CPU load", "Mem Usage", "Command"};
+std::array<std::string, NUM_FIELDS> process_field_names = {"Name","Pid", "User", "State", "Threads", "Start Time", "CPU Time", "CPU load", "Mem Usage", "Command"};
 
 std::mutex curser_lock;
 
 static int field_spacing = 0;
-int g_curser_x, g_curser_y;
+int input_curser_x, input_curser_y;
 
 static char get_proc_running_state(const PROCESS_STATE state) {
     if (state == PROCESS_STATE::RUNNING) return 'R';
@@ -50,7 +50,7 @@ static void nc_create_process_field_names() {
     
     wmove(process_field_w, 1,1);
     wattron(process_field_w, A_BOLD);
-    for (auto field_name : process_fields) {
+    for (auto field_name : process_field_names) {
         waddstr(process_field_w, field_name.c_str());
         waddstr(process_field_w, spacing.c_str());
     }
@@ -66,16 +66,34 @@ static int nc_move_curser_to_next_field(WINDOW *win, int field_index, const int 
     /* Calulate the sum of the characters 
      * in the previous fields */
     for (int i = 0; i <= field_index; ++i) {
-        field_pos += process_fields[i].size();
+        field_pos += process_field_names[i].size();
     }
 
     /* Calulate the sume of the spacing between the 
      * previous fields */
-    field_pos += field_spacing * (field_index + 1) + 1;
+    field_pos += field_spacing * (field_index + 1);
 
     wmove(win, y_pos, field_pos);
     field_index++;
 
+    return field_index;
+}
+
+static int nc_move_curser_to_previous_field(WINDOW *win, int field_index, const int y_pos) {
+
+    int field_pos = 0;
+    int current_x, current_y;
+
+
+    getyx(win, current_y, current_x);
+    
+    field_pos = current_x;
+    field_pos -= field_spacing;
+    field_pos -= process_field_names[field_index-1].size();
+
+    wmove(win, y_pos, field_pos);
+
+    field_index--;
     return field_index;
 }
 
@@ -126,7 +144,7 @@ static void nc_init() {
 
     int process_field_char_count = 0;
 
-    for (auto field_name : process_fields) {
+    for (auto field_name : process_field_names) {
         process_field_char_count += field_name.size();
     }
 
@@ -134,12 +152,8 @@ static void nc_init() {
 }
 
 void system_monitor_update(SystemMonitor& system_monitor, WINDOW* header_w, WINDOW* process_info_w) {
-
+    int current_curser_y, current_curser_x;
     while(1) {
-        curser_lock.lock();
-        getyx(process_info_w , g_curser_y, g_curser_x);
-        curser_lock.unlock();
-
         system_monitor.update();
         nc_print_header_info(header_w, system_monitor);
 
@@ -150,13 +164,16 @@ void system_monitor_update(SystemMonitor& system_monitor, WINDOW* header_w, WIND
             nc_print_process_info(process_info_w, process, y_pos);
             y_pos++;
             wmove(process_info_w, y_pos, 0);
-            wrefresh(process_info_w);
         }
-        wmove(process_info_w, g_curser_y, g_curser_x);
+        // Highlight the current line under the input curser
+        mvwchgat(process_info_w, input_curser_y, 0, -1, A_STANDOUT, 0, NULL);
+        // Restore the curser to the input curser
+        wmove(process_info_w, input_curser_y, input_curser_x);
         curser_lock.unlock();
 
         wrefresh(header_w);
         wrefresh(process_info_w);
+
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
@@ -175,27 +192,33 @@ int main() {
     keypad(process_info_w, TRUE);
 
     std::thread system_monitor_thread { system_monitor_update, std::ref(system_monitor), header_w, process_info_w};
-
+    int field_index = 0;
     while (1) {
-        int key;
-        getyx(process_info_w , g_curser_y, g_curser_x);
+        int key; 
+        getyx(process_info_w , input_curser_y, input_curser_x);
         key = wgetch(process_info_w);
+
         curser_lock.lock();
         switch (key) {
         case KEY_UP:
-            wmove(process_info_w, --g_curser_y, g_curser_x);
+            // Move up so clear current line highlighing
+            mvwchgat(process_info_w, input_curser_y, 0, -1, A_NORMAL, 0, NULL);
+            wmove(process_info_w, --input_curser_y, input_curser_x);
             break;
         case KEY_DOWN:
-            wmove(process_info_w, ++g_curser_y, g_curser_x);
+            // Move down so clear current line highlighing
+            mvwchgat(process_info_w, input_curser_y, 0, -1, A_NORMAL, 0, NULL);
+            wmove(process_info_w, ++input_curser_y, input_curser_x);
             break;
         case KEY_RIGHT:
-            wmove(process_info_w, g_curser_y, ++g_curser_x);
+            field_index = nc_move_curser_to_next_field(process_info_w, field_index, input_curser_y);
+            getyx(process_info_w , input_curser_y, input_curser_x);
             break;
         case KEY_LEFT:
-            wmove(process_info_w, g_curser_y, --g_curser_x);
+            field_index = nc_move_curser_to_previous_field(process_info_w, field_index, input_curser_y);
+            getyx(process_info_w , input_curser_y, input_curser_x);
             break;
         default:
-            //endwin();
             break;
         }
         wrefresh(process_info_w);
